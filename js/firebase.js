c0b844f69537c5fd21dadf586e3da876e5f60db0
ddbharathi <40708430+ddbharathi@users.noreply.github.com>
2026-05-21 14:49:40 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyC2AarE6wb92i14kE2Lly3I5PeYOKq_em8",
+  apiKey: "AIzaSyC2AarE6wb92i14kE2Lly3I5PeYOKq_em8",
   authDomain: "baby-store-a1b39.firebaseapp.com",
+  databaseURL: "https://baby-store-a1b39-default-rtdb.firebaseio.com",
   projectId: "baby-store-a1b39",
   storageBucket: "baby-store-a1b39.appspot.com",
   messagingSenderId: "452754922308",
-  appId: "1:452754922308:web:6dc7ff7f8294e7b21cd511",
+  appId: "1:452754922308:web:6dc7ff7f8294e7b21cd511"
 };
 
 const app = initializeApp(firebaseConfig);
