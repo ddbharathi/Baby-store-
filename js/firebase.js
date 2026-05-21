8aab15ac5bef59486b156116812be152f82c3a3a
ddbharathi <40708430+ddbharathi@users.noreply.github.com>
2026-05-21 12:50:17 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey:"AIzaSyC2AarE6wb92i14kE2Lly3I5PeYOKq_em8",
+  authDomain: "baby-store-a1b39.firebaseapp.com",
+  projectId: "baby-store-a1b39",
+  storageBucket: "baby-store-a1b39.appspot.com",
+  messagingSenderId: "452754922308",
+  appId: "1:452754922308:web:6dc7ff7f8294e7b21cd511",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
